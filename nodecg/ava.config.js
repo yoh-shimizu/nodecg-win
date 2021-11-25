145bd377bf1693ecef737bc8b8da8a69189c139b
Yohei Shimizu <yohei.shimizu@ietty.co.jp>
2021-11-25 21:57:49 +0900

    initilize

---
+++ nodecg/ava.config.js
@@ -0,0 +1,7 @@
+export default {
+	files: ['test/**'],
+	helpers: ['test/fixtures/**', 'test/helpers/**'],
+	concurrency: 1,
+	timeout: '2m',
+	verbose: true,
+};
---
+++ start.cmd
@@ -0,0 +1,6 @@
+@echo off
+set PATH=%PATH%;%cd%
+set NODE_PATH=%cd%\node_modules\npm\node_modules;%cd%\node_modules\npm
+cd nodecg
+node index.js
+cmd
